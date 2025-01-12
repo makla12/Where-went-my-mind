c8c5268a37c3199bf9995229dd7d861bd9a90257
pr1xt <timurkn2006@gmail.com>
2025-01-12 21:45:14 +0100

    Run mechanic fix

--- Assets/Scripts/FPSControler.cs
+++ Assets/Scripts/FPSControler.cs
@@ -39,10 +39,17 @@ public class FPSController : MonoBehaviour
         Vector3 right = transform.TransformDirection(Vector3.right);
 
         // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift) && characterController.isGrounded;
+        if (characterController.isGrounded && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+        }
 
 
-        
+
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
