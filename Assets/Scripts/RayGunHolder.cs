c2c63ed464286e71d55bce21f5a36858e54dd3ad
pr1xt <timurkn2006@gmail.com>
2025-01-14 22:26:46 +0100

    Gun Update V0.3

--- Assets/Scripts/RayGunHolder.cs
+++ Assets/Scripts/RayGunHolder.cs
@@ -91,7 +91,7 @@ public class GunSystem : MonoBehaviour
 
         //Graphics
         Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 180, 0));
-        Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+        // Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);     In case we want to add some effects from shoot
 
 
         bulletsLeft--;
