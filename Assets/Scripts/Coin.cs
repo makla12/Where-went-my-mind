6d01afcb44796aa076c33fd3383f8e36f116b051
Descaplis <92810856+Descaplis@users.noreply.github.com>
2025-04-04 23:42:31 +0200

    Coin animation

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -11,7 +11,7 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 100 * Time.deltaTime, 0); // Rotate the coin for visual effect
     }
 
     void OnCollisionEnter(Collision collision)
