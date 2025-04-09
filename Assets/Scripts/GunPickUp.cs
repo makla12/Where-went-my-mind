65c9c01e6dc7f2d0eee789a99408a27ed31602f8
makla12 <1234makla@gmail.com>
2025-04-09 13:56:16 +0200

    gun pick up update

--- Assets/Scripts/GunPickUp.cs
+++ Assets/Scripts/GunPickUp.cs
@@ -3,31 +3,40 @@ using UnityEngine;
 public class GunPickUp : MonoBehaviour
 {
     public GameObject gunVisualPrefab; 
+    public GameObject gunPrefab;
     private GameObject gunVisual; 
+    private InventoryControler inventoryControler; 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gunVisual = Instantiate(gunVisualPrefab, transform.position, Quaternion.identity);
+        gunVisual = Instantiate(gunVisualPrefab, transform);
+        gunVisual.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f); 
+        inventoryControler = GameObject.FindWithTag("Player").GetComponent<InventoryControler>();
     }
 
     void animateGun()
     {
         gunVisual.transform.position = new Vector3(transform.position.x, transform.position.y + Mathf.Sin(Time.time * 2) / 2, transform.position.z);
-        // spawnedGun.transform.Rotate(new Vector3(0, 0, 1), 10f * Time.deltaTime);
+        gunVisual.transform.Rotate(new Vector3(0, 1, 0), 40f * Time.deltaTime);
     }
 
     void Update()
     {
         animateGun();
-        if (Vector3.Distance(gunVisual.transform.position, Camera.main.transform.position) < 1f)
+        if (Vector3.Distance(gunVisual.transform.position, Camera.main.transform.position) < 3f)
         {
             // Check for input to pick up the gun
             if (Input.GetKeyDown(KeyCode.E))
             {
-                // Add the gun to the player's inventory or equip it
-                Debug.Log("Gun picked up!");
-                Destroy(gunVisual); // Destroy the gun after picking it up
+                if(inventoryControler.GetWeaponsCount() >= inventoryControler.maxWeapons)
+                {
+                    inventoryControler.ReplaceWeapon(gunVisualPrefab, gunPrefab, transform.position);
+                }
+                else
+                {
+                    inventoryControler.AddWeapon(gunVisualPrefab, gunPrefab);
+                }
+                Destroy(gameObject); 
             }
         }
 
